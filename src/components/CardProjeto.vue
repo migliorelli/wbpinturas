bd3cc39346e70af6e65ddd14968612d6e8c6078a
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-02-24 21:48:55 -0300

    added alt to image

--- src/components/CardProjeto.vue
+++ src/components/CardProjeto.vue
@@ -14,12 +14,13 @@ defineProps<Props>();
   >
     <img
       :src="imagem"
-      class="h-full transition-transform duration-300 group-hover:scale-110"
+      :alt="`$${titulo} - ${descricao}`"
+      class="h-full w-full object-cover transition-transform duration-300 group-hover:scale-110"
     />
     <div
-      class="absolute inset-0 flex h-full w-full flex-col items-start justify-end bg-gradient-to-t from-black/60 to-transparent p-6 md:opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+      class="absolute inset-0 flex h-full w-full flex-col items-start justify-end bg-gradient-to-t from-black/60 to-transparent p-6 text-start transition-opacity duration-300 group-hover:opacity-100 md:opacity-0"
     >
-      <p class="font-semibold text-white text-xl">{{ titulo }}</p>
+      <p class="text-xl font-semibold text-white">{{ titulo }}</p>
       <p class="text-white/80">{{ descricao }}</p>
     </div>
   </div>
