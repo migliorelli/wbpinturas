aee7e548a22d3016d6e73b856435f068712ee940
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-02-24 21:47:34 -0300

    added env config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  base: "/wbpinturas"
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [vue(), tailwindcss()],
+    base: env.URL_BASE,
+  };
 });
